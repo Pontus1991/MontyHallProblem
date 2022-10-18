f812c462f9025475b6c78958daa525f00d035627
Pontus1991 <70883949+Pontus1991@users.noreply.github.com>
2022-10-18 10:37:04 +0200

    - Add PlayerController - Add a public seedlist of players

---
+++ MontyHallProblem/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace MontyHallProblem
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MontyHallProblem/SeedData.cs
@@ -0,0 +1,18 @@
+﻿namespace MontyHallProblem
+{
+    public class SeedData
+    {
+        public List<Player> Players = new List<Player>() {
+            new Player
+            {
+                Id = 1, 
+                Name = "Player"
+            },
+            new Player
+            {
+                Id = 2,
+                Name = "Presenter"
+            }
+        };
+    }
+}
